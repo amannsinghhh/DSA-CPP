93950f94bebe092f7f92249037fc27e0274fbcc7
Aman Singh <72450073+amannsinghhh@users.noreply.github.com>
2023-09-02 21:33:11 +0530

    Create Search-in-Rotated-Sorted-Array.cpp

---
+++ Search-in-Rotated-Sorted-Array.cpp
@@ -0,0 +1,44 @@
+// 2September 2023
+//33. Search in Rotated Sorted Array
+//LC 33
+
+/*
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+*/
+
+class Solution {
+public:
+    int search(std::vector<int>& nums, int target) {
+        int s = 0, e = nums.size() - 1;
+
+        while (s <= e) {
+            int mid = (s + e) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[s] <= nums[mid]) {
+                if (nums[s] <= target && target < nums[mid]) {
+                    e = mid - 1;
+                } else {
+                    s = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[e]) {
+                    s = mid + 1;
+                } else {
+                    e = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
